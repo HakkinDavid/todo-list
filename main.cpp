ec2feba0edc32b1df85e09bf99868cf11f2796c6
HakkinDavid <desr11@hotmail.com>
2023-09-12 22:07:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main () {
 
     // individual tasks examples
     // actual program should implement these inside the yet to do ToDoList class
-    // constructor (description string, year, month, day, priority (0 is lowest), boolean completed )
+    // constructor (description string, year, month, day, priority (0 is lowest), boolean completed)
     Task t1 ("nose. si?", 2023, 9, 12);
     Task t2 ("si no?", 2023, 9, 16);
     Task t3 ("no?", 2023, 9, 11, 0, true);
