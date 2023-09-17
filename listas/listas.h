126f9fdb130d8de1b255d55cab36486238191e30
Red_Gunslinger <sofiathepeso@gmail.com>
2023-09-17 16:46:58 -0700

    listas.h comments
    my heart because you guys yell in the groupchat: </3

--- listas/listas.h
+++ listas/listas.h
@@ -12,35 +12,35 @@ using namespace std;
 template <class T>
 class Nodo {
 private:
-    T data;
-    Nodo<T>* next;
+    T data;//data guardado en el nodo
+    Nodo<T>* next;//pointer al proximo nodo en la lista
 
 public:
-    Nodo (T data, Nodo<T>* next = nullptr);
-    ~Nodo ();
+    Nodo (T data, Nodo<T>* next = nullptr);//constructor
+    ~Nodo ();//destructor
 
     void setData (T);
     void setNext (Nodo<T>*);
     T getData (void);
-    Nodo<T>* getNext (void);
+    Nodo<T>* getNext (void);//getter para el pointer al prox nodo en la litsa omg :O
 };
 
 template <class T>
 class Lista {
 private:
-    Nodo<T>* head;
+    Nodo<T>* head;//pointer al head (primer nodo) de la lista
 
 public:
-    Lista ();
-    ~Lista ();
+    Lista ();//constructor para initializar al obj lista con un list empty 
+    ~Lista ();//destructor 2 mai heart </3
 
-    void setHead (Nodo<T>*);
-    Nodo<T>* getHead (void);
+    void setHead (Nodo<T>*);//set head first node lista
+    Nodo<T>* getHead (void);//get head for list
 
-    void emplace_back (void);
-    void clear (void);
-    void insert (Nodo<T>*, T);
-    void erase (Nodo<T>*);
+    void emplace_back (void);//new nodo con datos defaulted al final de la lista
+    void clear (void);//clear list by haciendo set el head a un nullpointer
+    void insert (Nodo<T>*, T);//insert nodo nuevo con datos proovided despues del nodo especificdo en la lista 
+    void erase (Nodo<T>*);//erase specific nodo de la lista
 };
 
 #include "listas.cpp"
